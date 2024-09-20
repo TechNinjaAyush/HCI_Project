c99f51ed1125e8fc2ecc756041a4a59d9e4bbf70
Ksdhake28 <kdhake289@gmail.com>
2024-09-20 19:44:00 +0530

    changed

--- client/vite-project/components/Mainboard.jsx
+++ client/vite-project/components/Mainboard.jsx
@@ -1,13 +1,14 @@
-function Mainboard(){
-    return(
-        <>
-        <h2>Dashboard</h2>
-          <div class="promo_card">
-            <div class="subject-info">
-              <h3>Subject: HCI</h3>
-              <h3>Faculty : Khodskar</h3>
-            </div>
+function Mainboard() {
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <div class="promo_card">
+        <div class="subject-info">
+          <h3>Subject: HCI</h3>
+          <h3>Faculty : Khodskar</h3>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
+export default Mainboard;
